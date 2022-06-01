4b2b9985dcbed7eca38d96ab71373bd2450779f7
Welton Mota <welton.g.motta@gmail.com>
2022-06-01 17:38:39 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -6,10 +6,10 @@ import java.util.Objects;
 public class Department implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	private String name;
-
+	
 	public Department() {
 	}
 
